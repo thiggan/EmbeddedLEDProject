6d2452c194bc6374d93b2b3b41b8c70b6496a764
tracy macaulay-higgan <tracy@mad.geek.nz>
2023-03-30 10:27:27 +1300

    changes made to get more led strips working but some still to do to get it fully functional

--- sketch_mar29a_ProjectNewOne/sketch_mar29a_ProjectNewOne.ino
+++ sketch_mar29a_ProjectNewOne/sketch_mar29a_ProjectNewOne.ino
@@ -1,35 +1,60 @@
 #include <Adafruit_NeoPixel.h>
-#define PIN 13
+
+#define PIN_STRIP_0 13
+#define PIN_STRIP_1 12
+
 #define NUM_LEDS 6
 
-Adafruit_NeoPixel strip(NUM_LEDS, PIN);
+// todo : figure out how to put these in an array
+Adafruit_NeoPixel strip_0(NUM_LEDS, PIN_STRIP_0);
+Adafruit_NeoPixel strip_1(NUM_LEDS, PIN_STRIP_1);
 
 void setup() {
   Serial.begin(9600);
 
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  strip_0.begin();
+  strip_0.show(); 
+
+  strip_1.begin();
+  strip_1.show(); 
+}
+
+int readPortGetColorValue(int port)
+{
+  int value = analogRead(port);
+
+  Serial.print("Port '");
+  Serial.print(port);
+  Serial.print("' ");
+
+  Serial.print("value '");
+  Serial.print(value);
+  Serial.print("' ");
+
+  Serial.println("");
+
+  int colorValue = map(value, 0, 1024, 0, 255);
+  return colorValue;
 }
 
 void loop() {
  
-  int value = analogRead(A0);
-  Serial.println(value);
-
-  int colorValue = map(value, 0, 1024, 0, 255);
-  int color = strip.Color(colorValue*2, 255-colorValue*2, 0);
-
-  delay(100);
-
-  //strip.setPixelColor(0, color);
-
+  int value_strip_0 = readPortGetColorValue(A0);
+  int color_strip_0 = strip_0.Color(value_strip_0*2, 255-value_strip_0*2, 0);
   for (int i = 0; i < NUM_LEDS; i++) {
-    strip.setPixelColor(i, color);
+    strip_0.setPixelColor(i, color_strip_0);
   }
 
-  strip.show();
+  int value_strip_1 = readPortGetColorValue(A1);
+  int color_strip_1 = strip_0.Color(value_strip_1*2, 255-value_strip_0*1, 0);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    strip_1.setPixelColor(i, color_strip_1);
+  }
 
-  delay(100);
+  strip_0.show();
+  strip_1.show();
+
+  delay(500);
 
 }
 
