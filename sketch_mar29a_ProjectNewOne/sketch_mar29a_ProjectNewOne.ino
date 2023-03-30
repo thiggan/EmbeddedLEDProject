2984b756f3d27ff5ab947ac6c01630e5ea4e32ed
tracy macaulay-higgan <tracy@mad.geek.nz>
2023-03-30 16:45:59 +1300

    all 4 leds now working just need to work on the strength and colours

--- sketch_mar29a_ProjectNewOne/sketch_mar29a_ProjectNewOne.ino
+++ sketch_mar29a_ProjectNewOne/sketch_mar29a_ProjectNewOne.ino
@@ -1,13 +1,19 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN_STRIP_0 13
-#define PIN_STRIP_1 12
+#define PIN_STRIP_0 10
+#define PIN_STRIP_1 11
+#define PIN_STRIP_2 12
+#define PIN_STRIP_3 13
 
 #define NUM_LEDS 6
 
 // todo : figure out how to put these in an array
 Adafruit_NeoPixel strip_0(NUM_LEDS, PIN_STRIP_0);
 Adafruit_NeoPixel strip_1(NUM_LEDS, PIN_STRIP_1);
+Adafruit_NeoPixel strip_2(NUM_LEDS, PIN_STRIP_2);
+Adafruit_NeoPixel strip_3(NUM_LEDS, PIN_STRIP_3);
+
+
 
 void setup() {
   Serial.begin(9600);
@@ -17,6 +23,12 @@ void setup() {
 
   strip_1.begin();
   strip_1.show(); 
+
+  strip_2.begin();
+  strip_2.show(); 
+
+   strip_3.begin();
+   strip_3.show(); 
 }
 
 int readPortGetColorValue(int port)
@@ -51,8 +63,23 @@ void loop() {
     strip_1.setPixelColor(i, color_strip_1);
   }
 
+  int value_strip_2 = readPortGetColorValue(A2);
+  int color_strip_2 = strip_0.Color(value_strip_1*2, 255-value_strip_0*1, 0);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    strip_2.setPixelColor(i, color_strip_2);
+  }
+
+  int value_strip_3 = readPortGetColorValue(A3);
+  int color_strip_3 = strip_0.Color(value_strip_1*2, 255-value_strip_0*1, 0);
+  for (int i = 0; i < NUM_LEDS; i++) {
+    strip_3.setPixelColor(i, color_strip_3);
+  }
+
+
   strip_0.show();
   strip_1.show();
+  strip_2.show();
+  strip_3.show();
 
   delay(500);
 
