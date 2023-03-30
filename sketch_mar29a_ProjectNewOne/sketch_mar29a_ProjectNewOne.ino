48e51a7e9eaf0a97c989217056474a81e2a0eb42
tracy macaulay-higgan <tracy@mad.geek.nz>
2023-03-30 20:48:55 +1300

    function created to make a certain number of LEDs light up depending on the EMF output number

--- sketch_mar29a_ProjectNewOne/sketch_mar29a_ProjectNewOne.ino
+++ sketch_mar29a_ProjectNewOne/sketch_mar29a_ProjectNewOne.ino
@@ -21,14 +21,14 @@ void setup() {
   strip_0.begin();
   strip_0.show(); 
 
-  strip_1.begin();
-  strip_1.show(); 
+  // strip_1.begin();
+  // strip_1.show(); 
 
-  strip_2.begin();
-  strip_2.show(); 
+  // strip_2.begin();
+  // strip_2.show(); 
 
-   strip_3.begin();
-   strip_3.show(); 
+  //  strip_3.begin();
+  //  strip_3.show(); 
 }
 
 int readPortGetColorValue(int port)
@@ -60,44 +60,70 @@ int getColorFromValue(int value)
 }
 
 int ledLights(int number){
+  if(number > 0 && number < 200){
+    return 0;
+  }
+  else if(number > 201 && number < 340){
+    return 1;
+  }
+  else if(number > 341 && number < 480){
+    return 2;
+  }
+  else if(number > 481 && number < 620){
+    return 3;
+  }
+  else if( number > 621 && number < 750){
+    return 4;
+  }
+  else if(number > 751 && number < 890){
+    return 5;
+  }
+  else if(number > 891){
+    return 6;
+  }
+    
+  
 
- return 2;
 }
+
 
 void loop() {
  
   int value_strip_0 = readPortGetColorValue(A0);
-  int color_strip_0 = getColorFromValue(value_strip_0*2);
-  for (int i = 0; i < ledLights(value_strip_0); i++) {
+  int color_strip_0 = getColorFromValue(value_strip_0);
+  for (int i = 0; i < ledLights(value_strip_0)-1; i++) {
     strip_0.setPixelColor(i, color_strip_0);
-    
   }
 
-  int value_strip_1 = readPortGetColorValue(A1);
-  int color_strip_1 = strip_0.Color(value_strip_1*2, 255-value_strip_1*1, 0);
-  for (int i = 0; i < NUM_LEDS; i++) {
-    strip_1.setPixelColor(i, color_strip_1);
-  }
+  // strip_0.setPixelColor(0, strip_0.Color(255, 0, 0));
+  // strip_0.setPixelColor(1, strip_0.Color(0, 255, 0));
+  // strip_0.setPixelColor(2, strip_0.Color(0, 0, 255));
 
-  int value_strip_2 = readPortGetColorValue(A2);
-  int color_strip_2 = strip_0.Color(value_strip_2*2, 255-value_strip_2*1, 0);
-  for (int i = 0; i < NUM_LEDS; i++) {
-    strip_2.setPixelColor(i, color_strip_2);
-  }
+  // int value_strip_1 = readPortGetColorValue(A1);
+  // int color_strip_1 = strip_0.Color(value_strip_1*2, 255-value_strip_1*1, 0);
+  // for (int i = 0; i < NUM_LEDS; i++) {
+  //   strip_1.setPixelColor(i, color_strip_1);
+  // }
 
-  int value_strip_3 = readPortGetColorValue(A3);
-  int color_strip_3 = strip_0.Color(value_strip_3*2, 255-value_strip_3*1, 0);
-  for (int i = 0; i < NUM_LEDS; i++) {
-    strip_3.setPixelColor(i, color_strip_3);
-  }
+  // int value_strip_2 = readPortGetColorValue(A2);
+  // int color_strip_2 = strip_0.Color(value_strip_2*2, 255-value_strip_2*1, 0);
+  // for (int i = 0; i < NUM_LEDS; i++) {
+  //   strip_2.setPixelColor(i, color_strip_2);
+  // }
+
+  // int value_strip_3 = readPortGetColorValue(A3);
+  // int color_strip_3 = strip_0.Color(value_strip_3*2, 255-value_strip_3*1, 0);
+  // for (int i = 0; i < NUM_LEDS; i++) {
+  //   strip_3.setPixelColor(i, color_strip_3);
+  // }
 
 
   strip_0.show();
-  strip_1.show();
-  strip_2.show();
-  strip_3.show();
+  //strip_1.show();
+  //strip_2.show();
+  //strip_3.show();
 
-  delay(500);
+
 
 }
 
